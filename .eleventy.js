2a2edfa2b6801f6419e43d276f61964a43656fb1
PrivatePresley <nathan_townes_anderson@hotmail.com>
2023-02-23 21:43:29 -0500

    End of Class 7

---
+++ .eleventy.js
@@ -0,0 +1,15 @@
+module.exports = function (config) {
+  config.addPassthroughCopy("./src/css/");
+  config.addPassthroughCopy("./src/img/");
+  config.addPassthroughCopy("./src/js/");
+
+  return {
+    dir: {
+      input: "src",
+      output: "dist",
+    },
+  };
+};
+
+//Different syntax for export moduling, this is used usually for backend work
+//Tells us we're working in a Node application
